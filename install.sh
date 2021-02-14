f4b7904a3c23a7221c3179c3b88f7d0198bc4a57
ivnattt <60598561+ivnattt@users.noreply.github.com>
2021-02-14 14:27:49 +0530

    install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,11 @@
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+. ~/.nvm/nvm.sh
+
+nvm install node
+
+node -v
+npm -v
+
 sudo apt-get update 
 sudo apt-get install -y mongodb
 # sudo systemctl status mongodb
