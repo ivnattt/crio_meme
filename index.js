46414b69e7e7844b39dcbb5fe2c4ca8fa9ca565b
ivnattt <60598561+ivnattt@users.noreply.github.com>
2021-02-14 18:04:34 +0530

    button

--- index.js
+++ index.js
@@ -28,8 +28,6 @@ db.once('open', ()=> console.log('Connected to Mongoose'))
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
 
-const c = 'https://hdqwalls.com/wallpapers/best-nature-image.jpg'
-
 app.get('/', async(req, res) =>{
     try{
         const memes = await Meme.find({}).sort({createdAt: 'desc'}).limit(100)
@@ -39,6 +37,10 @@ app.get('/', async(req, res) =>{
     }
     
 })
+
+//----------------------------------------------------------------------------//
+//------------------------FOR CURL--------------------------------------------//
+//----------------------------------------------------------------------------//
 
 app.get('/memes', async(req, res) =>{
     try{
@@ -50,14 +52,15 @@ app.get('/memes', async(req, res) =>{
     
 })
 
-app.post('/', async(req, res) =>{
-    let meme = new Meme()
-    meme.name = req.body.name
-    meme.caption = req.body.caption
-    meme.link = req.body.link
-    await meme.save()
-    res.redirect('/')
+app.get('/memes/:id', async(req, res) => {
+    const meme = await Meme.findById(req.params.id)
+    if(meme){
+        res.json(meme)
+    } else {
+        res.status(400).send("404 Not Found")
+    }
 })
+
 
 app.post('/memes', async(req, res) =>{
     let meme = new Meme()
@@ -70,13 +73,35 @@ app.post('/memes', async(req, res) =>{
     //res.json(req.body)
 })
 
+//----------------------------------------------------------------------------//
+//----------------------------------------------------------------------------//
+
+app.get('/edit/:id', async(req, res) => {
+    const meme = await Meme.findById(req.params.id)
+    res.render('edit', {meme : meme})
+})
+
+app.post('/', async(req, res) =>{
+    let meme = new Meme()
+    meme.name = req.body.name
+    meme.caption = req.body.caption
+    meme.link = req.body.link
+    await meme.save()
+    res.redirect('/')
+})
+
 app.delete('/:id', async(req, res)=>{
     await Meme.findByIdAndDelete(req.params.id)
     res.redirect('/')
 })
 
-app.get('/meme', async(req, res) => {
-    res.render('meme', {c:c})
+app.put('/:id', async(req, res)=>{
+    let meme = await Meme.findById(req.params.id)
+    meme.name = req.body.name
+    meme.caption = req.body.caption
+    meme.link = req.body.link
+    await meme.save()
+    res.redirect('/')
 })
 
 app.listen(process.env.PORT || 8081)
